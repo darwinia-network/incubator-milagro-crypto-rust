7a7d37afd3ac73f1170a9ce3bcef8919ad449a92
echo <hujw77@gmail.com>
2022-06-02 14:50:28 +0800

    Fix missing

--- src/std.rs
+++ src/std.rs
@@ -1,5 +1,5 @@
 #[cfg(feature = "std")]
-pub use std::{borrow, cmp, fmt, format, time, vec, vec::Vec};
+pub use std::{borrow, cmp, fmt, format, str, string, time, vec, vec::Vec};
 
 #[cfg(not(feature = "std"))]
 pub use alloc::{borrow, format, string, vec, vec::Vec};
